540212cffd77265136b15049bdc0c4c290679c9d
Yamagn <syankenpon@gmail.com>
2020-04-23 14:24:39 +0900

    cropとか圧縮とかできるように変更

--- mp42gif/mp42gif.py
+++ mp42gif/mp42gif.py
@@ -4,7 +4,15 @@ import os
 import shutil
 from PIL import Image
 
-def mp42images(video_path, dir_path, basename, ext='jpg'):
+def crop_center(im, crop_width, crop_height):
+    img_width, img_height = im.size
+    return im.crop(((img_width - crop_width) // 2,
+                   (img_height - crop_height) // 2,
+                   (img_width + crop_width) // 2,
+                   (img_height + crop_height) // 2))
+
+def mp42images(video_path, basename, quality, crop, ext='jpg'):
+    dir_path = 'result'
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         return
@@ -15,30 +23,45 @@ def mp42images(video_path, dir_path, basename, ext='jpg'):
     digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
     n = 0
     while True:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, n)
         ret, frame = cap.read()
+        if quality == "high":
+            rate = 1
+        elif quality == "middle":
+            rate = 3
+        elif quality == "low":
+            rate = 5
+        else:
+            rate = 1
         if ret:
-            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame)
+            im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            if crop == "center":
+                im = crop_center(im, 1120, 630)
+            resized_frame = im.resize((int(im.width / rate), int(im.height / rate)))
+            resized_frame.save('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext))
             n += 1
         else:
             return int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-def images2gif(dir_path, basename, length ,ext='jpg'):
+def images2gif(dir_path, basename, length, ext='jpg'):
     imgs = []
     digit = len(str(length))
-    base_path = os.path.join(dir_path, basename)
+    images_path = 'result'
+    base_path = os.path.join(images_path, basename)
+    save_dir = os.path.join(dir_path, basename)
     n = 0
-    for i in range(length):
-        im = Image.open('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext))
-        n += 1
-        imgs.append(im.copy())
-    imgs[0].save('test.gif', save_all=True, append_images=imgs[1:], duration=30, loop=0)
+    while True:
+        if n < length:
+            im = Image.open('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext))
+            n += 1
+            imgs.append(im.copy())
+        else:
+            break
+    imgs[0].save('{}.gif'.format(save_dir), save_all=True, append_images=imgs[1:], duration=30, loop=0)
     shutil.rmtree('result')
 
 
 
-def mp4togif(video_path, basename):
-    dir_path = 'result'
-    length = mp42images(video_path, dir_path, basename)
-    images2gif(dir_path, basename, length)
-
-mp4togif("F:\\User\Videos\\Final Fantasy XIV  A Realm Reborn\\Final Fantasy XIV  A Realm Reborn 2020.04.12 - 22.22.31.02.DVR_Trim.mp4", "sample")
+def mp4togif(video_path, dir_path, basename, quarity="high", crop="none", ext="jpg"):
+    length = mp42images(video_path, basename, quarity, crop, ext)
+    images2gif(dir_path, basename, length, ext)
